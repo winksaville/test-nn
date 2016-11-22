eb4f71e8a47fac022a95f67bd8eee4c08c9970bc
Wink Saville <wink@saville.com>
2016-11-21 18:22:35 -0800

    Change the bounding_box min/max value.
    Setting the bounding_box this way gives me consistent colors through out
    the run when using VisIT as the actual values range from -10.x to +10.x.

--- NeuralNetIo.c
+++ NeuralNetIo.c
@@ -91,7 +91,7 @@ static Status begin_epoch(NeuralNetIoWriter* writer, size_t epoch) {
   }
 
   // Write header
-  char* bounding_box = "0.0 0.0 -12.0 0.0\n1.0 1.0 12.0 0.0\n";
+  char* bounding_box = "0.0 0.0 -12.0 -12.0\n1.0 1.0 12.0 12.0\n";
   status = writer->write_str(writer, bounding_box);
   if (StatusErr(status)) {
     printf("NeuralNetIoWriter_init: unable to write bounding_box\n");
